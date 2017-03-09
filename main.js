8bc2eca88403ad6640318a4dd5b164a6bef019f1
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-08 21:41:14 -0500

    basic game functional

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ var current_letter = "X"
 
 $('.clickable').on('click', clickEvent)
 
+function checkIfAllOccupied(id1, id2, id3, id4, id5, id6, id7, id8, id9) {
+  return $(id1).html() !== "" &&
+         $(id2).html() !== "" &&
+         $(id3).html() !== "" &&
+         $(id4).html() !== "" &&
+         $(id5).html() !== "" &&
+         $(id6).html() !== "" &&
+         $(id7).html() !== "" &&
+         $(id8).html() !== "" &&
+         $(id9).html() !== "";
+}
+
 function checkIfEmpty(id1, id2, id3) {
   return $(id1).html() !== "" && $(id2).html() !== "" && $(id3).html() !== "";
 }
@@ -14,30 +26,40 @@ function checkIfWon() {
   switch(true) {
     case checkIfEqual(sq1, sq2, sq3) && checkIfEmpty(sq1, sq2, sq3):
       youWin(current_letter);
+      break;
 
     case checkIfEqual(sq1, sq4, sq7) && checkIfEmpty(sq1, sq4, sq7):
       youWin(current_letter);
+      break;
 
     case checkIfEqual(sq1, sq5, sq9) && checkIfEmpty(sq1, sq5, sq9):
       youWin(current_letter);
+      break;
 
     case checkIfEqual(sq2, sq5, sq8) && checkIfEmpty(sq2, sq5, sq8):
       youWin(current_letter);
+      break;
 
     case checkIfEqual(sq3, sq6, sq9) && checkIfEmpty(sq3, sq6, sq9):
       youWin(current_letter);
+      break;
 
     case checkIfEqual(sq3, sq5, sq7) && checkIfEmpty(sq3, sq5, sq7):
       youWin(current_letter);
+      break;
 
     case checkIfEqual(sq4, sq5, sq6) && checkIfEmpty(sq4, sq5, sq6):
       youWin(current_letter);
+      break;
 
     case checkIfEqual(sq7, sq8, sq9) && checkIfEmpty(sq7, sq8, sq9):
       youWin(current_letter);
+      break;
 
-    case checkIfEmpty(sq7, sq8, sq9) && checkIfEmpty(sq7, sq8, sq9):
-      youWin(current_letter);
+    case checkIfAllOccupied(sq1, sq2, sq3, sq4, sq5, sq6, sq7, sq8, sq9):
+      $('#messages').html("It's a draw! Play again?");
+      $('.display-none').removeClass('display-none');
+      break;
 
     default:
       break;
@@ -65,7 +87,7 @@ function startNewGame() {
 
 function youWin(letter) {
   $('#messages').html(letter + " player won!");
-  $('.display-none').removeClass('display-none')
+  $('.display-none').removeClass('display-none');
   for (i = 0; i < 9; i++) {
     $('#sq' + (i + 1)).removeClass('clickable').off('click');
   }
