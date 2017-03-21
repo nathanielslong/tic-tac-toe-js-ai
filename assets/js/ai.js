f67ba6e6054a39141e079df3e6bc69d3d6d11366
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-03-21 13:47:12 -0400

    basic AI actions added

--- assets/js/ai.js
+++ assets/js/ai.js
@@ -38,3 +38,48 @@ var AI = function(level) {
     }
   }
 }
+
+// The next function represents the actions of the AI, taken into its own class for modularity. It has two main roles: remembering where on the board a play is, and the minimax value calculated.
+
+var AIAction = function(pos) {
+  // specify defaults
+  this.movePosition = pos;
+  this.minimaxValue = 0;
+
+  // Applies an action to the board to advance to the next state
+  this.applyTo = function(state) {
+    var next = new State(state);
+
+    next.board[this.movePosition] = state.turn;
+
+    if (state.turn == "O") {
+      next.oMovesCount++;
+    }
+
+    next.advanceTurn();
+
+    return next;
+  }
+}
+
+// Sorting methods for minimaxValues are below. Basically, in order to determine the best outcome, we need to be able to sort given probabilities by best and worst. we return -1, 1, and 0 in order for JavaSCript's array.sort function to sort them properly.
+
+AIAction.ASCENDING = function(firstAction, secondAction) {
+  if (firstAction.minimaxVal < secondAction.minimaxVal) {
+    return -1;
+  } else if (firstAction.minimaxVal > secondAction.minimaxVal) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+AIAction.DESCENDING = function(firstAction, secondAction) {
+  if (firstAction.minimaxVal > secondAction.minimaxVal) {
+    return -1;
+  } else if (firstAction.minimaxVal < secondAction.minimaxVal) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
